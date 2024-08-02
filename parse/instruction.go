dbf91ae878c440b394a5754e039a325053733d18
Alon Krymgand <me@alon.kr>
2024-08-02 21:20:14 +0300

    Added InstructionParser

--- parse/instruction.go
+++ parse/instruction.go
@@ -1,18 +1,19 @@
 package parse
 
 import (
+	"usm/lex"
 	"usm/source"
 )
 
 type InstructionNode struct {
-	Operation source.UnmanagedSourceView
+	Operator  source.UnmanagedSourceView
 	Arguments []CallerArgumentNode
 	Targets   []RegisterNode
 }
 
 func (n InstructionNode) View() source.UnmanagedSourceView {
-	first := n.Operation
-	last := n.Operation
+	first := n.Operator
+	last := n.Operator
 
 	if len(n.Targets) > 0 {
 		first = n.Targets[0].View()
@@ -51,6 +52,60 @@ func (n InstructionNode) stringTargets(ctx source.SourceContext) (s string) {
 }
 
 func (n InstructionNode) String(ctx source.SourceContext) string {
-	op := string(n.Operation.Raw(ctx))
+	op := string(n.Operator.Raw(ctx))
 	return n.stringTargets(ctx) + op + n.stringArguments(ctx)
+}
+
+type InstructionParser struct{}
+
+func (InstructionParser) parseTargets(v *TokenView, node *InstructionNode) {
+	for {
+		reg, err := RegisterParser{}.Parse(v)
+		if err != nil {
+			return
+		}
+		node.Targets = append(node.Targets, reg)
+	}
+}
+
+func (InstructionParser) parseEquals(v *TokenView, node *InstructionNode) (err ParsingError) {
+	if len(node.Targets) > 0 {
+		_, err = v.ConsumeToken(lex.EqlToken)
+	}
+	return
+}
+
+func (InstructionParser) parseOperator(v *TokenView, node *InstructionNode) ParsingError {
+	opr, err := v.ConsumeToken(lex.OprToken)
+	node.Operator = opr.View
+	return err
+}
+
+func (InstructionParser) parseArguments(v *TokenView, node *InstructionNode) ParsingError {
+	for {
+		arg, err := CallerArgumentParser{}.Parse(v)
+		if err != nil {
+			break
+		}
+		node.Arguments = append(node.Arguments, arg)
+	}
+
+	_, err := v.ConsumeToken(lex.SepToken)
+	return err
+}
+
+func (p InstructionParser) Parse(v *TokenView) (node InstructionNode, err ParsingError) {
+	p.parseTargets(v, &node)
+	err = p.parseEquals(v, &node)
+	if err != nil {
+		return
+	}
+
+	err = p.parseOperator(v, &node)
+	if err != nil {
+		return
+	}
+
+	err = p.parseArguments(v, &node)
+	return
 }
