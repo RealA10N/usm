c662eae37c542d66e7c0925bd8f583e97c329c5a
Alon Krymgand <me@alon.kr>
2024-08-02 21:39:39 +0300

    Added BlockParser

--- parse/block.go
+++ parse/block.go
@@ -1,6 +1,7 @@
 package parse
 
 import (
+	"usm/lex"
 	"usm/source"
 )
 
@@ -22,4 +23,29 @@ func (n BlockNode) String(ctx source.SourceContext) (s string) {
 	}
 	s += "}\n"
 	return s
+}
+
+type BlockParser struct{}
+
+func (p BlockParser) Parse(v *TokenView) (node BlockNode, err ParsingError) {
+	start, err := v.ConsumeToken(lex.LcrToken)
+	if err != nil {
+		return
+	}
+
+	for {
+		inst, err := InstructionParser{}.Parse(v)
+		if err != nil {
+			break
+		}
+		node.Instructions = append(node.Instructions, inst)
+	}
+
+	end, err := v.ConsumeToken(lex.RcrToken)
+	if err != nil {
+		return
+	}
+
+	node.UnmanagedSourceView = start.View.Merge(end.View)
+	return node, nil
 }
