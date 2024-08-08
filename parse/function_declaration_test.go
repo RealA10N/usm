3e04a16d6a4b2c78929214c4afb99ed18e7fc25b
Alon Krymgand <me@alon.kr>
2024-08-09 01:43:36 +0300

    Modified Function Declaration to Language Spec

--- parse/function_declaration_test.go
+++ parse/function_declaration_test.go
@@ -12,15 +12,15 @@ import (
 
 func TestSignatureParserOnlyIdentifier(t *testing.T) {
 	v, ctx := source.NewSourceView("@fibonacci").Detach()
-	glb := lex.Token{Type: lex.GlbToken, View: v}
+	glb := lex.Token{Type: lex.GlobalToken, View: v}
 	tknView := parse.NewTokenView([]lex.Token{glb})
 
-	expectedSig := parse.SignatureNode{
+	expectedSig := parse.FunctionDeclarationNode{
 		UnmanagedSourceView: v.Subview(0, 10),
 		Identifier:          v,
 	}
 
-	sig, err := parse.SignatureParser{}.Parse(&tknView)
+	sig, err := parse.FunctionDeclarationParser{}.Parse(&tknView)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedSig, sig)
 
@@ -30,12 +30,12 @@ func TestSignatureParserOnlyIdentifier(t *testing.T) {
 
 func TestSignatureParserVoidFunction(t *testing.T) {
 	v, ctx := source.NewSourceView("... @printNumber  $i32 %x ...").Detach()
-	glb := lex.Token{Type: lex.GlbToken, View: v.Subview(4, 16)}
-	typ := lex.Token{Type: lex.TypToken, View: v.Subview(18, 22)}
-	reg := lex.Token{Type: lex.RegToken, View: v.Subview(23, 25)}
+	glb := lex.Token{Type: lex.GlobalToken, View: v.Subview(4, 16)}
+	typ := lex.Token{Type: lex.TypeToken, View: v.Subview(18, 22)}
+	reg := lex.Token{Type: lex.RegisterToken, View: v.Subview(23, 25)}
 	tknView := parse.NewTokenView([]lex.Token{glb, typ, reg})
 
-	expectedSig := parse.SignatureNode{
+	expectedSig := parse.FunctionDeclarationNode{
 		UnmanagedSourceView: v.Subview(4, 25),
 		Identifier:          glb.View,
 		Parameters: []parse.ParameterNode{
@@ -46,7 +46,7 @@ func TestSignatureParserVoidFunction(t *testing.T) {
 		},
 	}
 
-	sig, err := parse.SignatureParser{}.Parse(&tknView)
+	sig, err := parse.FunctionDeclarationParser{}.Parse(&tknView)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedSig, sig)
 	assert.Equal(t, v.Subview(4, 25), sig.View())
@@ -55,17 +55,17 @@ func TestSignatureParserVoidFunction(t *testing.T) {
 
 func TestSignatureParserSingleReturn(t *testing.T) {
 	v, ctx := source.NewSourceView("$i32 @add $i32 %x $i32 %y").Detach()
-	ret := lex.Token{Type: lex.TypToken, View: v.Subview(0, 4)}
-	id := lex.Token{Type: lex.GlbToken, View: v.Subview(5, 9)}
-	param1Typ := lex.Token{Type: lex.TypToken, View: v.Subview(10, 14)}
-	param1Reg := lex.Token{Type: lex.RegToken, View: v.Subview(15, 17)}
-	param2Typ := lex.Token{Type: lex.TypToken, View: v.Subview(18, 22)}
-	param2Reg := lex.Token{Type: lex.RegToken, View: v.Subview(23, 25)}
+	ret := lex.Token{Type: lex.TypeToken, View: v.Subview(0, 4)}
+	id := lex.Token{Type: lex.GlobalToken, View: v.Subview(5, 9)}
+	param1Typ := lex.Token{Type: lex.TypeToken, View: v.Subview(10, 14)}
+	param1Reg := lex.Token{Type: lex.RegisterToken, View: v.Subview(15, 17)}
+	param2Typ := lex.Token{Type: lex.TypeToken, View: v.Subview(18, 22)}
+	param2Reg := lex.Token{Type: lex.RegisterToken, View: v.Subview(23, 25)}
 	tknView := parse.NewTokenView([]lex.Token{
 		ret, id, param1Typ, param1Reg, param2Typ, param2Reg,
 	})
 
-	expectedSig := parse.SignatureNode{
+	expectedSig := parse.FunctionDeclarationNode{
 		UnmanagedSourceView: v,
 		Identifier:          id.View,
 		Parameters: []parse.ParameterNode{
@@ -83,7 +83,7 @@ func TestSignatureParserSingleReturn(t *testing.T) {
 		},
 	}
 
-	sig, err := parse.SignatureParser{}.Parse(&tknView)
+	sig, err := parse.FunctionDeclarationParser{}.Parse(&tknView)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedSig, sig)
 	assert.Equal(t, v, sig.View())
@@ -92,18 +92,18 @@ func TestSignatureParserSingleReturn(t *testing.T) {
 
 func TestSignatureParserMutltiReturn(t *testing.T) {
 	v, ctx := source.NewSourceView("$i32  $i32  @divmod  $i32 %n  $i32 %m").Detach()
-	ret1 := lex.Token{Type: lex.TypToken, View: v.Subview(0, 4)}
-	ret2 := lex.Token{Type: lex.TypToken, View: v.Subview(6, 10)}
-	id := lex.Token{Type: lex.GlbToken, View: v.Subview(12, 19)}
-	param1Typ := lex.Token{Type: lex.TypToken, View: v.Subview(21, 25)}
-	param1Reg := lex.Token{Type: lex.RegToken, View: v.Subview(26, 28)}
-	param2Typ := lex.Token{Type: lex.TypToken, View: v.Subview(30, 34)}
-	param2Reg := lex.Token{Type: lex.RegToken, View: v.Subview(35, 37)}
+	ret1 := lex.Token{Type: lex.TypeToken, View: v.Subview(0, 4)}
+	ret2 := lex.Token{Type: lex.TypeToken, View: v.Subview(6, 10)}
+	id := lex.Token{Type: lex.GlobalToken, View: v.Subview(12, 19)}
+	param1Typ := lex.Token{Type: lex.TypeToken, View: v.Subview(21, 25)}
+	param1Reg := lex.Token{Type: lex.RegisterToken, View: v.Subview(26, 28)}
+	param2Typ := lex.Token{Type: lex.TypeToken, View: v.Subview(30, 34)}
+	param2Reg := lex.Token{Type: lex.RegisterToken, View: v.Subview(35, 37)}
 	tknView := parse.NewTokenView([]lex.Token{
 		ret1, ret2, id, param1Typ, param1Reg, param2Typ, param2Reg,
 	})
 
-	expectedSig := parse.SignatureNode{
+	expectedSig := parse.FunctionDeclarationNode{
 		UnmanagedSourceView: v,
 		Identifier:          id.View,
 		Parameters: []parse.ParameterNode{
@@ -122,7 +122,7 @@ func TestSignatureParserMutltiReturn(t *testing.T) {
 		},
 	}
 
-	sig, err := parse.SignatureParser{}.Parse(&tknView)
+	sig, err := parse.FunctionDeclarationParser{}.Parse(&tknView)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedSig, sig)
 	assert.Equal(t, v, sig.View())
@@ -131,15 +131,15 @@ func TestSignatureParserMutltiReturn(t *testing.T) {
 
 func TestSignatureParserIdentifierNotGlobal(t *testing.T) {
 	v, _ := source.NewSourceView("funcid").Detach()
-	opr := lex.Token{Type: lex.OprToken, View: v}
+	opr := lex.Token{Type: lex.OperatorToken, View: v}
 	tknView := parse.NewTokenView([]lex.Token{opr})
 
 	expectedErr := parse.UnexpectedTokenError{
-		Expected: []lex.TokenType{lex.GlbToken},
+		Expected: []lex.TokenType{lex.GlobalToken},
 		Actual:   opr,
 	}
 
-	_, err := parse.SignatureParser{}.Parse(&tknView)
+	_, err := parse.FunctionDeclarationParser{}.Parse(&tknView)
 	assert.NotNil(t, err)
 
 	details, ok := err.(parse.UnexpectedTokenError)
