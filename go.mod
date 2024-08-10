fb5541162d5804014a3d7e326a00506d7c818c00
Alon Krymgand <me@alon.kr>
2024-08-10 16:53:03 +0300

    Now using cobra for CLI

--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.22.5
 
 require (
 	alon.kr/x/view v0.0.0-20240810101401-7b9dabbb2b19
+	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.9.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- usm.go
+++ usm.go
@@ -4,46 +4,72 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/spf13/cobra"
+
 	"alon.kr/x/usm/lex"
 	"alon.kr/x/usm/parse"
 	"alon.kr/x/usm/source"
 )
 
 func main() {
-	file, ok := os.Open("a.usm")
-	if ok != nil {
-		panic("can't open file")
-	}
-	defer file.Close()
-
-	tokenizer := lex.NewTokenizer()
-	view, err := source.ReadSource(file)
-	if err != nil {
-		panic("can't read file")
+	rootCmd := &cobra.Command{
+		Use:   "usm",
+		Short: " One Universal assembly language to rule them all.",
 	}
 
-	tokens, err := tokenizer.Tokenize(view)
-	if err != nil {
-		panic(err)
+	lexCmd := &cobra.Command{
+		Use:   "lex",
+		Short: "Lex the source code",
+		Run: func(cmd *cobra.Command, args []string) {
+			view, err := source.ReadSource(os.Stdin)
+			if err != nil {
+				panic(err)
+			}
+
+			tokens, err := lex.NewTokenizer().Tokenize(view)
+			if err != nil {
+				panic(err)
+			}
+
+			_, ctx := view.Detach()
+			for _, tkn := range tokens {
+				fmt.Printf("%s ", tkn.String(ctx))
+				if tkn.Type == lex.SeparatorToken {
+					fmt.Println()
+				}
+			}
+		},
 	}
 
-	fmt.Println("=== Tokens ===")
+	fmtCmd := &cobra.Command{
+		Use:   "fmt",
+		Short: "Format the source code",
+		Run: func(cmd *cobra.Command, args []string) {
+			view, err := source.ReadSource(os.Stdin)
+			if err != nil {
+				panic(err)
+			}
 
-	_, ctx := view.Detach()
-	for _, tkn := range tokens {
-		fmt.Printf("%s ", tkn.String(ctx))
-		if tkn.Type == lex.SeparatorToken {
-			fmt.Println()
-		}
+			tokens, err := lex.NewTokenizer().Tokenize(view)
+			if err != nil {
+				panic(err)
+			}
+
+			tknView := parse.NewTokenView(tokens)
+			file, perr := parse.NewFileParser().Parse(&tknView)
+			if perr == nil {
+				fmt.Print(file.String(view.Ctx()))
+			} else {
+				fmt.Println(perr.Error(view.Ctx()))
+			}
+		},
 	}
 
-	fmt.Println("\n=== Formatted Source ===")
+	rootCmd.AddCommand(lexCmd)
+	rootCmd.AddCommand(fmtCmd)
 
-	tknView := parse.NewTokenView(tokens)
-	fn, perr := parse.NewFileParser().Parse(&tknView)
-	if perr == nil {
-		fmt.Print(fn.String(ctx))
-	} else {
-		fmt.Println(perr.Error(ctx))
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
