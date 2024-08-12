a56656c22eb9b54781f6b59795fa7de72976ace4
Alon Krymgand <me@alon.kr>
2024-08-12 18:50:00 +0300

    Added TypeDecorator parsing support

--- parse/type.go
+++ parse/type.go
@@ -5,17 +5,34 @@ import (
 	"alon.kr/x/usm/source"
 )
 
-type TypeNode struct {
+// MARK: Node
+
+type TypeDecoratorType uint8
+
+const (
+	PointerTypeDecorator TypeDecoratorType = iota
+	RepeatTypeDecorator
+)
+
+type TypeDecorator struct {
 	source.UnmanagedSourceView
+	Type TypeDecoratorType
+}
+
+type TypeNode struct {
+	Identifier source.UnmanagedSourceView
+	Decorators []TypeDecorator
 }
 
 func (n TypeNode) View() source.UnmanagedSourceView {
-	return n.UnmanagedSourceView
+	return n.Identifier
 }
 
 func (n TypeNode) String(ctx source.SourceContext) string {
-	return string(n.UnmanagedSourceView.Raw(ctx))
+	return string(n.Identifier.Raw(ctx))
 }
+
+// MARK: Parser
 
 type TypeParser struct{}
 
@@ -23,11 +40,36 @@ func (TypeParser) String() string {
 	return "$type"
 }
 
-func (TypeParser) Parse(v *TokenView) (node TypeNode, err ParsingError) {
+func (p TypeParser) parseDecorator(v *TokenView, node *TypeNode) (err ParsingError) {
+	tkn, err := v.ConsumeToken(lex.PointerToken, lex.RepeatToken)
+	if err != nil {
+		return
+	}
+
+	decorator := TypeDecorator{UnmanagedSourceView: tkn.View}
+	switch tkn.Type {
+	case lex.PointerToken:
+		decorator.Type = PointerTypeDecorator
+	case lex.RepeatToken:
+		decorator.Type = RepeatTypeDecorator
+	default:
+		panic("unreachable")
+	}
+
+	node.Decorators = append(node.Decorators, decorator)
+	return nil
+}
+
+func (p TypeParser) Parse(v *TokenView) (node TypeNode, err ParsingError) {
 	tkn, err := v.ConsumeToken(lex.TypeToken)
 	if err != nil {
 		return
 	}
 
-	return TypeNode{tkn.View}, nil
+	node.Identifier = tkn.View
+	for err == nil {
+		err = p.parseDecorator(v, &node)
+	}
+
+	return node, nil
 }
