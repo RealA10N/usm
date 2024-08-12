7a1bf540a06f32c1862d4f51c203d56a1b714b42
Alon Krymgand <me@alon.kr>
2024-08-12 14:27:09 +0300

    Added Type Declaration Parser & Node

---
+++ parse/type_declaration.go
@@ -0,0 +1,77 @@
+package parse
+
+import (
+	"alon.kr/x/usm/lex"
+	"alon.kr/x/usm/source"
+)
+
+// MARK: Node
+
+type TypeDeclarationNode struct {
+	source.UnmanagedSourceView
+	Identifier source.UnmanagedSourceView
+	Fields     BlockNode[TypeFieldNode]
+}
+
+func (n TypeDeclarationNode) View() source.UnmanagedSourceView {
+	return n.UnmanagedSourceView
+}
+
+func (n TypeDeclarationNode) String(ctx source.SourceContext) string {
+	return "type " + string(n.Identifier.Raw(ctx)) + " " + n.Fields.String(ctx)
+}
+
+// MARK: Parser
+
+type TypeDeclarationParser struct {
+	FieldsParser BlockParser[TypeFieldNode]
+}
+
+func (TypeDeclarationParser) String() string {
+	return "type declaration"
+}
+
+func (TypeDeclarationParser) parseTypeKeyword(v *TokenView, node *TypeDeclarationNode) (err ParsingError) {
+	kw, err := v.ConsumeToken(lex.TypeKeywordToken)
+	if err != nil {
+		return
+	}
+
+	node.Start = kw.View.Start
+	return
+}
+
+func (p TypeDeclarationParser) parseIdentifier(v *TokenView, node *TypeDeclarationNode) (err ParsingError) {
+	id, err := v.ConsumeToken(lex.TypeToken)
+	if err != nil {
+		return
+	}
+
+	node.Identifier = id.View
+	return
+}
+
+func (p TypeDeclarationParser) parseBlock(v *TokenView, node *TypeDeclarationNode) (err ParsingError) {
+	node.Fields, err = p.FieldsParser.Parse(v)
+	if err != nil {
+		return
+	}
+
+	node.End = node.Fields.End
+	return
+}
+
+func (p TypeDeclarationParser) Parse(v *TokenView) (node TypeDeclarationNode, err ParsingError) {
+	err = p.parseTypeKeyword(v, &node)
+	if err != nil {
+		return
+	}
+
+	err = p.parseIdentifier(v, &node)
+	if err != nil {
+		return
+	}
+
+	err = p.parseBlock(v, &node)
+	return
+}
---
+++ parse/type_field.go
@@ -0,0 +1,14 @@
+package parse
+
+import "alon.kr/x/usm/source"
+
+type TypeFieldNode struct {
+}
+
+func (n TypeFieldNode) View() (v source.UnmanagedSourceView) {
+	return // TODO: not implemented
+}
+
+func (n TypeFieldNode) String(ctx source.SourceContext) (s string) {
+	return // TODO: not implemented
+}
