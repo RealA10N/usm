6bedd5da076463ca1260cb5b92164999a6aae881
Alon Krymgand <me@alon.kr>
2024-08-12 15:16:04 +0300

    Added PeekToken method

--- parse/token_view.go
+++ parse/token_view.go
@@ -12,6 +12,30 @@ func NewTokenView(tkns []lex.Token) TokenView {
 	return TokenView{view.NewView[lex.Token, uint32](tkns)}
 }
 
+func (v *TokenView) PeekToken(expectedTypes ...lex.TokenType) (tkn lex.Token, perr ParsingError) {
+	tkn, err := v.At(0)
+
+	if err != nil {
+		perr = EofError{Expected: expectedTypes}
+		return
+	}
+
+	gotExpected := false
+	for _, expectedType := range expectedTypes {
+		if tkn.Type == expectedType {
+			gotExpected = true
+			break
+		}
+	}
+
+	if !gotExpected {
+		perr = UnexpectedTokenError{Expected: expectedTypes, Actual: tkn}
+		return
+	}
+
+	return tkn, nil
+}
+
 func (v *TokenView) ConsumeToken(expectedTypes ...lex.TokenType) (tkn lex.Token, perr ParsingError) {
 	tknView, restView := v.Partition(1)
 	tkn, err := tknView.At(0)
