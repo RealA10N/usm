970ef5bdd5f8d8a89f70cd8356d202296c60122f
Alon Krymgand <me@alon.kr>
2024-08-13 14:12:03 +0300

    Added GlobalDeclarationParser

--- parse/global_declaration.go
+++ parse/global_declaration.go
@@ -32,3 +32,42 @@ func (n GlobalDeclarationNode) View() source.UnmanagedSourceView {
 		return n.Identifier.View().MergeEnd((*n.Immediate).View())
 	}
 }
+
+// MARK: Parser
+
+type GlobalDeclarationParser struct {
+	GlobalParser         GlobalParser
+	TypeParser           TypeParser
+	ImmediateValueParser *ImmediateValueParser
+}
+
+func NewGlobalDeclarationParser() GlobalDeclarationParser {
+	return GlobalDeclarationParser{
+		ImmediateValueParser: NewImmediateValueParser(),
+	}
+}
+
+func (p *GlobalDeclarationParser) Parse(v *TokenView) (
+	node GlobalDeclarationNode,
+	err ParsingError,
+) {
+	node.Identifier, err = p.GlobalParser.Parse(v)
+	if err != nil {
+		return
+	}
+
+	node.Type, err = p.TypeParser.Parse(v)
+	if err != nil {
+		return
+	}
+
+	immediate, err := p.ImmediateValueParser.Parse(v)
+	if err == nil {
+		// TODO: improve error catching. Not every error here means that there
+		// is no immediate value! what if an immediate block is provided (the
+		// next token is '{') but there is an error parsing the ImmediateBlock?
+		node.Immediate = &immediate
+	}
+
+	return node, nil
+}
