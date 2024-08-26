e9f22e26b16a3f22d67133a565d6c8f88b551e4d
Alon Krymgand <me@alon.kr>
2024-08-27 00:24:03 +0300

    Fixed instruction test

--- parse/instruction_test.go
+++ parse/instruction_test.go
@@ -11,28 +11,34 @@ import (
 )
 
 func TestInstructionParserMultipleTargets(t *testing.T) {
-	srcView := source.NewSourceView("%div %mod = divmod %x %y")
+	srcView := source.NewSourceView("$32 %div $32 %mod = divmod %x %y")
 	unmanaged := srcView.Unmanaged()
 
 	expected := parse.InstructionNode{
-		Operator: unmanaged.Subview(12, 18),
+		Operator: unmanaged.Subview(20, 26),
 		Arguments: []parse.ArgumentNode{
-			parse.RegisterNode{unmanaged.Subview(19, 21)},
-			parse.RegisterNode{unmanaged.Subview(22, 24)},
+			parse.RegisterNode{unmanaged.Subview(27, 29)},
+			parse.RegisterNode{unmanaged.Subview(30, 32)},
 		},
-		Targets: []parse.RegisterNode{
-			{unmanaged.Subview(0, 4)},
-			{unmanaged.Subview(5, 9)},
+		Targets: []parse.ParameterNode{
+			{
+				Type:     parse.TypeNode{Identifier: unmanaged.Subview(0, 3)},
+				Register: parse.RegisterNode{unmanaged.Subview(4, 8)},
+			},
+			{
+				Type:     parse.TypeNode{Identifier: unmanaged.Subview(9, 12)},
+				Register: parse.RegisterNode{unmanaged.Subview(13, 17)},
+			},
 		},
 	}
 
-	expectedString := "\t%div %mod = divmod %x %y\n"
+	expectedString := "\t$32 %div $32 %mod = divmod %x %y\n"
 
 	testExpectedInstruction(t, srcView, expected, expectedString)
 }
 
 func TestInstructionWithImmediateValuesAndLabel(t *testing.T) {
-	srcView := source.NewSourceView(".entry %res = add %x $32 #1 .arg")
+	srcView := source.NewSourceView(".entry $32 %res = add %x $32 #1 .arg")
 	unmanaged := srcView.Unmanaged()
 
 	expected := parse.InstructionNode{
@@ -47,8 +53,11 @@ func TestInstructionWithImmediateValuesAndLabel(t *testing.T) {
 			},
 			parse.LabelNode{unmanaged.Subview(28, 32)},
 		},
-		Targets: []parse.RegisterNode{
-			{unmanaged.Subview(7, 11)},
+		Targets: []parse.ParameterNode{
+			{
+				Type:     parse.TypeNode{Identifier: unmanaged.Subview(14, 17)},
+				Register: parse.RegisterNode{unmanaged.Subview(18, 20)},
+			},
 		},
 		Labels: []parse.LabelNode{
 			{unmanaged.Subview(0, 6)},
