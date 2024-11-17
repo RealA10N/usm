d980f28011cfebd474bb6ef283421f031e0e709e
Alon Krymgand <me@alon.kr>
2024-11-17 16:47:54 +0200

    Added base `UsmUint`

---
+++ core/uint.go
@@ -0,0 +1,6 @@
+package core
+
+// Base type for unsigned integers.
+// This defines, for example, the maximum length of a source file, and the
+// maximum type size, and other bounds for internal data structures.
+type UsmUint = uint32
