19937908e04b951f095f3b6ceeb29c27b6044fed
Alon Krymgand <me@alon.kr>
2024-11-17 17:20:00 +0200

    Added `ParseUint` to `core`

--- core/uint.go
+++ core/uint.go
@@ -1,6 +1,18 @@
 package core
 
+import (
+	"strconv"
+	"unsafe"
+)
+
 // Base type for unsigned integers.
 // This defines, for example, the maximum length of a source file, and the
 // maximum type size, and other bounds for internal data structures.
 type UsmUint = uint32
+
+const UsmUintBitSize = 8 * unsafe.Sizeof(UsmUint(0))
+
+func ParseUint(s string) (UsmUint, error) {
+	n, err := strconv.ParseUint(s, 10, int(UsmUintBitSize))
+	return UsmUint(n), err
+}
