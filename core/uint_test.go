163c95e5250cce853871f779d2cb02ce80c0f21d
Alon Krymgand <me@alon.kr>
2024-11-18 23:04:25 +0200

    Added `core.ParseUint` tests

---
+++ core/uint_test.go
@@ -0,0 +1,29 @@
+package core_test
+
+import (
+	"testing"
+
+	"alon.kr/x/usm/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseUintDecimal(t *testing.T) {
+	n, err := core.ParseUint("123")
+	assert.Nil(t, err)
+	assert.Equal(t, core.UsmUint(123), n)
+}
+
+// Currently, the expected behavior is to not support other bases.
+// Parsing currently fails if provided no decimal digit runes (this is the
+// expected behavior).
+// TODO: consider adding support for other bases?
+
+func TestParseUintBinaryFail(t *testing.T) {
+	_, err := core.ParseUint("0b101")
+	assert.NotNil(t, err)
+}
+
+func TestParseUintPadding(t *testing.T) {
+	_, err := core.ParseUint(" 123   ")
+	assert.NotNil(t, err)
+}
