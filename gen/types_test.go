5adbc792366bb84ab14434544a06c93da8040b08
Alon Krymgand <me@alon.kr>
2024-11-18 11:46:06 +0200

    Added type pointer type declaration test

--- gen/types_test.go
+++ gen/types_test.go
@@ -15,7 +15,7 @@ func (m TypeManager) GetType(name string) *gen.TypeInfo {
 	return m[name]
 }
 
-func (m TypeManager) constructType(name string, size core.UsmUint) {
+func (m TypeManager) registerBuiltinType(name string, size core.UsmUint) {
 	m.RegisterType(name, &gen.TypeInfo{Size: size})
 }
 
@@ -33,8 +33,7 @@ func (m TypeManager) RegisterType(name string, typ *gen.TypeInfo) core.UsmError 
 
 func TestBasicTypeAliasDeclaration(t *testing.T) {
 	typeManager := make(TypeManager)
-	typeManager.constructType("$32", 4)
-	typeManager.constructType("$64", 8)
+	typeManager.registerBuiltinType("$32", 4)
 
 	view := core.NewSourceView("type $myType $32")
 	unmanaged := view.Unmanaged()
@@ -58,10 +57,51 @@ func TestBasicTypeAliasDeclaration(t *testing.T) {
 		SourceContext: view.Ctx(),
 		Types:         typeManager,
 	}
-	typeInfo, err := gen.TypeInfoFromTypeDeclaration(&genCtx, typeDeclarationNode)
 
+	typeInfo, err := gen.TypeInfoFromTypeDeclaration(&genCtx, typeDeclarationNode)
 	assert.Nil(t, err)
 	assert.NotNil(t, typeInfo)
 	assert.Equal(t, "$myType", string(typeInfo.Name.Raw(genCtx.SourceContext)))
 	assert.EqualValues(t, 4, typeInfo.Size)
+}
+
+func TestBasicTypeAliasPointerDeclaration(t *testing.T) {
+	typeManager := make(TypeManager)
+	typeManager.registerBuiltinType("$64", 8)
+
+	view := core.NewSourceView("type $myType $64 *")
+	unmanaged := view.Unmanaged()
+
+	typeDeclarationNode := parse.TypeDeclarationNode{
+		UnmanagedSourceView: unmanaged,
+		Identifier:          unmanaged.Subview(5, 12),
+		Fields: parse.BlockNode[parse.TypeFieldNode]{
+			UnmanagedSourceView: core.UnmanagedSourceView{},
+			Nodes: []parse.TypeFieldNode{
+				{
+					Type: parse.TypeNode{
+						Identifier: unmanaged.Subview(13, 16),
+						Decorators: []parse.TypeDecoratorNode{
+							{
+								UnmanagedSourceView: unmanaged.Subview(17, 18),
+								Type:                parse.PointerTypeDecorator,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	genCtx := gen.GenerationContext{
+		SourceContext: view.Ctx(),
+		ArchInfo:      gen.ArchInfo{PointerSize: 1337},
+		Types:         typeManager,
+	}
+
+	typeInfo, err := gen.TypeInfoFromTypeDeclaration(&genCtx, typeDeclarationNode)
+	assert.Nil(t, err)
+	assert.NotNil(t, typeInfo)
+	assert.Equal(t, "$myType", string(typeInfo.Name.Raw(genCtx.SourceContext)))
+	assert.EqualValues(t, 1337, typeInfo.Size)
 }
