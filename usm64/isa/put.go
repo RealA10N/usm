066e800d364d82b269fe66c92577d9b44a8fd2dc
Alon Krymgand <me@alon.kr>
2024-12-13 13:54:41 +0200

    `PutInstruction` now expected `ValuedArgument`

--- usm64/isa/put.go
+++ usm64/isa/put.go
@@ -3,6 +3,7 @@ package usm64isa
 import (
 	"fmt"
 
+	"alon.kr/x/list"
 	"alon.kr/x/usm/core"
 	"alon.kr/x/usm/gen"
 	usm64core "alon.kr/x/usm/usm64/core"
@@ -11,10 +12,12 @@ import (
 // MARK: Debug
 
 type PutInstruction struct {
-	Argument usm64core.Argument
+	Argument usm64core.ValuedArgument
 }
 
-func (i *PutInstruction) Emulate(ctx *usm64core.EmulationContext) usm64core.EmulationError {
+func (i *PutInstruction) Emulate(
+	ctx *usm64core.EmulationContext,
+) usm64core.EmulationError {
 	fmt.Println(i.Argument.Value(ctx))
 	ctx.NextInstructionIndex++
 	return nil
@@ -24,9 +27,19 @@ func NewPutInstruction(
 	targets []usm64core.Register,
 	argument []usm64core.Argument,
 ) (usm64core.Instruction, core.ResultList) {
-	return &PutInstruction{
-		Argument: argument[0],
-	}, core.ResultList{}
+	valued, ok := argument[0].(usm64core.ValuedArgument)
+	if !ok {
+		v := argument[0].Declaration()
+		return nil, list.FromSingle(core.Result{
+			{
+				Type:     core.ErrorResult,
+				Message:  "Expected valued argument",
+				Location: &v,
+			},
+		})
+	}
+
+	return &PutInstruction{Argument: valued}, core.ResultList{}
 }
 
 func NewPutInstructionDefinition() gen.InstructionDefinition[usm64core.Instruction] {
