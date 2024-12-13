6ad54ccd4a280afda69b6b58166214684c7537d9
Alon Krymgand <me@alon.kr>
2024-12-13 13:17:35 +0200

    Added `Declaration` field to `gen.ArgumentInfo`

--- gen/argument.go
+++ gen/argument.go
@@ -9,9 +9,13 @@ import (
 // MARK: Info
 
 type ArgumentInfo interface {
-	// A pointer to the TypeInfo instance that corresponds to the type of the
-	// register.
-	GetType() ReferencedTypeInfo
+	// A pointer to the ReferencedTypeInfo instance that corresponds to the
+	// type of the argument. Nil if the argument does not have a type (for
+	// example, a label).
+	GetType() *ReferencedTypeInfo
+
+	// The location where the argument appears in the source code.
+	Declaration() core.UnmanagedSourceView
 }
 
 // MARK: Generator
