1329c93b219400426ccfc7f03a36c842900e2e99
Alon Krymgand <me@alon.kr>
2024-12-17 21:18:46 +0200

    Moved `control_flow` package to root

--- control_flow/builder.go
+++ control_flow/builder.go
@@ -1,22 +1,5 @@
 package control_flow
 
-type SupportsControlFlow interface {
-	// Returns a list of all instruction indices in the function that execution
-	// could arrive to after the execution of this instruction.
-	PossibleNextInstructionIndices() []uint
-}
-
-type ControlFlowBasicBlock struct {
-	InstructionIndices []uint
-	ForwardEdges       []uint
-	BackwardEdges      []uint
-}
-
-type ControlFlowGraph[InstT SupportsControlFlow] struct {
-	Instructions []InstT
-	BasicBlocks  []ControlFlowBasicBlock
-}
-
 func getInstructionsForwardEdges[InstT SupportsControlFlow](
 	instructions []InstT,
 ) [][]uint {
