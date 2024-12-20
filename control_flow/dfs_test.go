7159fd6b245d9d3f99f8b5bd0c9688e14021485c
Alon Krymgand <me@alon.kr>
2024-12-20 14:31:38 +0200

    Added special edges tests

--- control_flow/dfs_test.go
+++ control_flow/dfs_test.go
@@ -45,3 +45,27 @@ func TestBinaryTree(t *testing.T) {
 	//                    index: 0  1  2  3  4  5  6
 	assert.EqualValues(t, []uint{0, 1, 4, 2, 3, 5, 6}, preorder)
 }
+
+func TestSpecialEdges(t *testing.T) {
+	//    0      with an additional cross edge 5 -> 1
+	//  1   2    a back edge 4 -> 0
+	// 3 4 5 6   and a forward edge 0 -> 6
+
+	cfg := control_flow.ControlFlowGraph[*TestInstruction]{
+		BasicBlocks: []control_flow.ControlFlowBasicBlock{
+			{ForwardEdges: []uint{1, 2, 6}}, // 0
+			{ForwardEdges: []uint{3, 4}},    // 1
+			{ForwardEdges: []uint{5, 6}},    // 2
+			{ForwardEdges: []uint{}},        // 3
+			{ForwardEdges: []uint{0}},       // 4
+			{ForwardEdges: []uint{1}},       // 5
+			{ForwardEdges: []uint{}},        // 6
+		},
+	}
+
+	preorder := cfg.PreOrderDfs()
+	assert.Len(t, preorder, len(cfg.BasicBlocks))
+	//                    index: 0  1  2  3  4  5  6
+	assert.EqualValues(t, []uint{0, 1, 4, 2, 3, 5, 6}, preorder)
+
+}
