1398bf757813e94651cedfbc716b5abbf2cc6d68
Alon Krymgand <me@alon.kr>
2024-12-20 21:43:19 +0200

    Implemented `eval` for dominator tree construction

--- control_flow/dominator_tree_builder.go
+++ control_flow/dominator_tree_builder.go
@@ -15,3 +15,55 @@
 // https://pfalcon.github.io/ssabook/latest/book-full.pdf
 
 package control_flow
+
+type dominatorTreeBuilder struct {
+	SemiDom []uint
+	Parent  []uint
+}
+
+func (b *dominatorTreeBuilder) isRoot(v uint) bool {
+	return v == b.Parent[v]
+}
+
+func (b *dominatorTreeBuilder) isDirectAncestorOfRoot(v uint) bool {
+	return b.isRoot(b.Parent[v])
+}
+
+// eval(v) returns v if and only if it is the root in a tree of the forest.
+// Otherwise, it returns a node u in the path from v to the root of the tree
+// that contains v (not including the root itself), such that Semidominator(u)
+// is minimal.
+//
+// Since we are calculating the semidominators of vertices in reverse order,
+// if evaluating a node that has yet to be processed and given a semidominator,
+// the tree containing that node will be a singleton and thus it will be the
+// root, and the node itself will be returned by definition.
+//
+// If however, we evaluate a node that has already been processed, it must not
+// be the root of the tree it is in, since right after processing a node we
+// merge it with its parent. Then, the second case in the eval definition
+// applies, and we iterate over the ancestors of the node in the tree until
+// reaching the root, returning the node with the minimal semidominator.
+// On the way, we compress the path to the root of the tree, to speed up future
+// evaluations.
+//
+// For reference, see Henrik Knakkegaard Christensen's master's thesis, section
+// 3.3, page 27:
+// https://users-cs.au.dk/gerth/advising/thesis/henrik-knakkegaard-christensen.pdf
+func (b *dominatorTreeBuilder) eval(v uint) (minNode uint, root uint) {
+	if b.isRoot(v) {
+		return v, v
+	}
+
+	if b.isDirectAncestorOfRoot(v) {
+		return v, b.Parent[v]
+	}
+
+	minNode, root = b.eval(b.Parent[v])
+	b.Parent[v] = root
+	if b.SemiDom[v] < b.SemiDom[minNode] {
+		minNode = v
+	}
+
+	return minNode, root
+}
