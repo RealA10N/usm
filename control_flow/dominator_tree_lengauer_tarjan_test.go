2cd2c7a9fb16d4224dd548937103c5e229cba499
Alon Krymgand <me@alon.kr>
2024-12-22 20:40:21 +0200

    Moved example test graph to separate file

--- control_flow/dominator_tree_lengauer_tarjan_test.go
+++ control_flow/dominator_tree_lengauer_tarjan_test.go
@@ -16,22 +16,13 @@ func TestIfElseDominatorTreeBuilder(t *testing.T) {
 	//     3
 	//
 	// dominator tree:
-	//
 	//      0
 	//    / | \
 	//   1  2  3
 
-	cfg := control_flow.Graph{
-		Nodes: []control_flow.Node{
-			{ForwardEdges: []uint{1, 2}, BackwardEdges: []uint{}}, // 0
-			{ForwardEdges: []uint{3}, BackwardEdges: []uint{0}},   // 1
-			{ForwardEdges: []uint{3}, BackwardEdges: []uint{0}},   // 2
-			{ForwardEdges: []uint{}, BackwardEdges: []uint{1, 2}}, // 3
-		},
-	}
+	cfg := control_flow.NewGraph([][]uint{{1, 2}, {3}, {3}, {}})
+	dominatorTree := cfg.DominatorTree(0)
 
-	entryNode := uint(0)
-	dominatorTree := cfg.DominatorTree(entryNode)
 	expectedImmDom := []uint{0, 0, 0, 0}
 	assert.EqualValues(t, expectedImmDom, dominatorTree.ImmDom)
 }
@@ -39,35 +30,10 @@ func TestIfElseDominatorTreeBuilder(t *testing.T) {
 func TestKnakkegaardsDominatorTreeExample(t *testing.T) {
 	// Example taken from Knakkegaard Thesis, section 2.6, page 14:
 	// https://users-cs.au.dk/gerth/advising/thesis/henrik-knakkegaard-christensen.pdf
-	//
-	// control flow graph:
-	//       0
-	//      ↙ ↘
-	//     1   2
-	//    ↙ ↖ ↙ ↘
-	//   3   4   5
-	//    ↖←←←←←↙
-	//
-	// dominator tree:
-	//       0
-	//     / | \
-	//    1  2  3
-	//      / \
-	//     4   5
 
-	cfg := control_flow.Graph{
-		Nodes: []control_flow.Node{
-			{ForwardEdges: []uint{1, 2}, BackwardEdges: []uint{}},  // 0
-			{ForwardEdges: []uint{3}, BackwardEdges: []uint{0, 4}}, // 1
-			{ForwardEdges: []uint{4, 5}, BackwardEdges: []uint{0}}, // 2
-			{ForwardEdges: []uint{}, BackwardEdges: []uint{1, 5}},  // 3
-			{ForwardEdges: []uint{1}, BackwardEdges: []uint{2}},    // 4
-			{ForwardEdges: []uint{3}, BackwardEdges: []uint{2}},    // 5
-		},
-	}
+	cfg := KnakkegaardGraphExample()
+	dominatorTree := cfg.DominatorTree(0)
 
-	entryNode := uint(0)
-	dominatorTree := cfg.DominatorTree(entryNode)
 	expectedImmDom := []uint{0, 0, 0, 0, 2, 2}
 	assert.EqualValues(t, expectedImmDom, dominatorTree.ImmDom)
 }
@@ -76,40 +42,13 @@ func TestLengauerTarjansPaperDominatorTreeExample(t *testing.T) {
 	// Example taken from Lengauers's & Tarjan's paper, figures 1 & 2.
 	// https://doi.org/10.1145/357062.357071
 
+	cfg := LengauerTarjanGraphExample()
+	dominatorTree := cfg.DominatorTree(0)
 	R := uint(0)
-	A := uint(1)
-	B := uint(2)
 	C := uint(3)
 	D := uint(4)
-	E := uint(5)
-	F := uint(6)
 	G := uint(7)
-	H := uint(8)
-	I := uint(9)
-	J := uint(10)
-	K := uint(11)
-	L := uint(12)
 
-	cfg := control_flow.Graph{
-		Nodes: []control_flow.Node{
-			{ForwardEdges: []uint{A, B, C}, BackwardEdges: []uint{K}},    // R
-			{ForwardEdges: []uint{D}, BackwardEdges: []uint{R, B}},       // A
-			{ForwardEdges: []uint{A, D, E}, BackwardEdges: []uint{R}},    // B
-			{ForwardEdges: []uint{F, G}, BackwardEdges: []uint{R}},       // C
-			{ForwardEdges: []uint{L}, BackwardEdges: []uint{A, B}},       // D
-			{ForwardEdges: []uint{H}, BackwardEdges: []uint{H, B}},       // E
-			{ForwardEdges: []uint{I}, BackwardEdges: []uint{C}},          // F
-			{ForwardEdges: []uint{I, J}, BackwardEdges: []uint{C}},       // G
-			{ForwardEdges: []uint{E, K}, BackwardEdges: []uint{L, E}},    // H
-			{ForwardEdges: []uint{K}, BackwardEdges: []uint{K, F, G, J}}, // I
-			{ForwardEdges: []uint{I}, BackwardEdges: []uint{G}},          // J
-			{ForwardEdges: []uint{R, I}, BackwardEdges: []uint{H, I}},    // K
-			{ForwardEdges: []uint{H}, BackwardEdges: []uint{D}},          // L
-		},
-	}
-
-	entryNode := uint(0)
-	dominatorTree := cfg.DominatorTree(entryNode)
 	//                       R  A  B  C  D  E  F  G  H  I  J  K  L
 	expectedImmDom := []uint{R, R, R, R, R, R, C, C, R, R, G, R, D}
 	assert.EqualValues(t, expectedImmDom, dominatorTree.ImmDom)
@@ -143,20 +82,9 @@ func TestSsaBookDominatorTreeExample(t *testing.T) {
 	//        |   |
 	//        5   3
 
-	cfg := control_flow.Graph{
-		Nodes: []control_flow.Node{
-			{ForwardEdges: []uint{1, 6}, BackwardEdges: []uint{}},  // 0
-			{ForwardEdges: []uint{2, 4}, BackwardEdges: []uint{0}}, // 1
-			{ForwardEdges: []uint{3}, BackwardEdges: []uint{1}},    // 2
-			{ForwardEdges: []uint{4}, BackwardEdges: []uint{2}},    // 3
-			{ForwardEdges: []uint{5}, BackwardEdges: []uint{1, 3}}, // 4
-			{ForwardEdges: []uint{6}, BackwardEdges: []uint{4}},    // 5
-			{ForwardEdges: []uint{0}, BackwardEdges: []uint{0, 5}}, // 6
-		},
-	}
+	cfg := SSABookGraphExample()
+	dominatorTree := cfg.DominatorTree(0)
 
-	entryNode := uint(0)
-	dominatorTree := cfg.DominatorTree(entryNode)
 	//                       0  1  2  3  4  5  6
 	expectedImmDom := []uint{0, 0, 1, 2, 1, 4, 0}
 	assert.EqualValues(t, expectedImmDom, dominatorTree.ImmDom)
@@ -166,28 +94,8 @@ func TestDjGraphPaperDominatorTreeExample(t *testing.T) {
 	// Example taken from Sreedhar's & Gao's paper that first introduced
 	// DJ-Graphs (figure 1): https://doi.org/10.1145/199448.199464
 
-	cfg := control_flow.NewGraph([][]uint{
-		{1, 16},     // 0 (START)
-		{2, 3, 4},   // 1
-		{4, 7},      // 2
-		{9},         // 3
-		{5},         // 4
-		{6},         // 5
-		{2, 8},      // 6
-		{8},         // 7
-		{7, 15},     // 8
-		{10, 11},    // 9
-		{12},        // 10
-		{12},        // 11
-		{13},        // 12
-		{3, 14, 15}, // 13
-		{12},        // 14
-		{16},        // 15
-		{},          // 16 (END)
-	})
-
-	entryNode := uint(0)
-	dominatorTree := cfg.DominatorTree(entryNode)
+	cfg := SreedharGaoGraphExample()
+	dominatorTree := cfg.DominatorTree(0)
 	//                       0  1  2  3  4  5  6  7  8  9 10 11 12  13  14 15 16
 	expectedImmDom := []uint{0, 0, 1, 1, 1, 4, 5, 1, 1, 3, 9, 9, 9, 12, 13, 1, 0}
 	assert.EqualValues(t, expectedImmDom, dominatorTree.ImmDom)
