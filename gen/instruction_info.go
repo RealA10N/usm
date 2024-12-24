8641e3131ef73b71370d1edf46594fd605fd1a70
Alon Krymgand <me@alon.kr>
2024-12-24 17:51:18 +0200

    Updated instruction information struct

--- gen/instruction_info.go
+++ gen/instruction_info.go
@@ -1,13 +1,19 @@
 package gen
 
-type InstructionInfo[InstT BaseInstruction] struct {
-	// The actual instruction instance, which is part of the instruction set,
-	// and not part of this package.
-	Instruction InstT
+import "alon.kr/x/usm/core"
 
+type InstructionInfo struct {
 	// The targets of the instruction.
 	Targets []*RegisterArgumentInfo
 
 	// The arguments of the instruction.
 	Arguments []ArgumentInfo
+
+	// The labels that point directly to this instruction.
+	Labels []*LabelInfo[BaseInstruction]
+
+	// The location in which the instruction was defined in the source code.
+	// Can be nil if the instruction was defined internally, for example,
+	// in an optimization.
+	Declaration *core.UnmanagedSourceView
 }
