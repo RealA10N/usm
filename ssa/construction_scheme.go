2e85f86c8083ef2e2a3d81694910a4c24a2879da
Alon Krymgand <me@alon.kr>
2024-12-31 19:10:24 +0200

    Added `SsaConstructionScheme` interfaces

---
+++ ssa/construction_scheme.go
@@ -0,0 +1,51 @@
+package ssa
+
+import (
+	"alon.kr/x/usm/core"
+	"alon.kr/x/usm/gen"
+)
+
+type ReachingDefinitionsSet interface {
+	// Provided an original base register, this function returns the reaching
+	// renamed register definition that originates from the base register.
+	//
+	// This function should be called via an implementation of the
+	// RenameBasicBlock method, in which the reaching definition is the unique
+	// definition of the register that reaches (dominates) the entry of the
+	// basic block. Note that this does not include definitions of the register
+	// in the basic block itself, and it is up to the implementation to track.
+	// This DOES include definitions of the register in inserted phi functions,
+	// including phi functions in the entry this basic block.
+	GetReachingDefinition(base *gen.RegisterInfo) (renamed *gen.RegisterInfo)
+}
+
+type PhiInstruction interface {
+	AddForwardingRegister(*gen.BasicBlockInfo, *gen.RegisterInfo)
+}
+
+// This interface defines the process of renaming registers in the SSA
+// construction process. Each ISA should implement this interface to provide
+// support for the SSA construction process.
+type SsaConstructionScheme interface {
+	// Inserts a new phi instruction in the provided basic block, which is used
+	// to define the provided (already renamed) register.
+	NewPhiInstruction(*gen.BasicBlockInfo, *gen.RegisterInfo) PhiInstruction
+
+	// Creates a new unique register that is used as a renaming of the provided
+	// register in the construction of the SSA form.
+	//
+	// It is on the implementation to provide a unique register creation scheme,
+	// which can create multiple new registers from the same base register.
+	// The implementation must not return the same new register for the same
+	// base register on different called, i.e. it should create a new register
+	// every time it is called.
+	NewRenamedRegister(*gen.RegisterInfo) *gen.RegisterInfo
+
+	// Provided a basic block and a set of the reaching definitions to that
+	// basic block, the implementation should rename all registers that are
+	// used and defined in the basic block.
+	//
+	// The caller does not grantee the order of basic blocks in which the calls
+	// to this method are made.
+	RenameBasicBlock(*gen.BasicBlockInfo, ReachingDefinitionsSet) core.ResultList
+}
