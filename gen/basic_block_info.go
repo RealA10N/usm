db0b81e40d4e748f9e49374f85837953cf292783
Alon Krymgand <me@alon.kr>
2025-01-03 18:35:49 +0200

    Added `BasicBlockInfo.GetRepresentingLabel` function

--- gen/basic_block_info.go
+++ gen/basic_block_info.go
@@ -22,6 +22,23 @@ func (i *BasicBlockInfo) String() string {
 	return s
 }
 
+// Get a single label instance that represents the basic block, if it exists.
+// If the basic block has multiple labels, the function will return one of
+// them arbitrary.
+// If the block does not have labels pointing to it, returns nil.
+func (i *BasicBlockInfo) GetRepresentingLabel() *LabelInfo {
+	firstInstruction := i.Instructions[0]
+	labels := firstInstruction.Labels
+
+	if len(labels) < 1 {
+		// TODO: in this case, we should generate a new label, using some sort of
+		// "LabelManager".
+		return nil
+	}
+
+	return labels[0]
+}
+
 func NewEmptyBasicBlockInfo() *BasicBlockInfo {
 	return &BasicBlockInfo{
 		Instructions: []*InstructionInfo{},
