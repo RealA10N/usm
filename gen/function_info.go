a7d4aff80e1d55b11dbd6349ef1c74d61d71b04d
Alon Krymgand <me@alon.kr>
2025-01-03 17:11:19 +0200

    Added `String` method for `gen.FunctionInfo`

--- gen/function_info.go
+++ gen/function_info.go
@@ -1,8 +1,33 @@
 package gen
 
 type FunctionInfo struct {
+	Name       string
 	EntryBlock *BasicBlockInfo
 	Registers  []*RegisterInfo
 	Parameters []*RegisterInfo
 	Targets    []ReferencedTypeInfo
+}
+
+func (i *FunctionInfo) String() string {
+	s := "func "
+	for _, target := range i.Targets {
+		s += target.String() + " "
+	}
+
+	s += i.Name + " "
+
+	for _, param := range i.Parameters {
+		s += param.String() + " "
+	}
+
+	s += "{\n"
+
+	block := i.EntryBlock
+	for block != nil {
+		s += block.String()
+		block = block.NextBlock
+	}
+
+	s += "}"
+	return s
 }
