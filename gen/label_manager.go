1dba637571e19f56407c8cf2af891b6a1b78d1d2
Alon Krymgand <me@alon.kr>
2025-01-03 18:05:21 +0200

    `LabelManager` now needs new `GenerateLabel` method

--- gen/label_manager.go
+++ gen/label_manager.go
@@ -3,6 +3,25 @@ package gen
 import "alon.kr/x/usm/core"
 
 type LabelManager interface {
+	// Get a information about a label from its name.
+
+	// Create a new label, provided it's information.
+	// This should register the label in the label manager database as a valid
+	// label, and future queries should be able to find it.
 	GetLabel(name string) *LabelInfo
+
+	// Generate a new label that will be used with the provided block.
+	// This should create a new label with a unique name, but it SHOULDN'T add
+	// it to the labels database (does not call NewLabel).
+	GenerateLabel(block *BasicBlockInfo) *LabelInfo
+
+	// Create a new label, provided it's information.
+	// This should register the label in the label manager database as a valid
+	// label, and future queries should be able to find it.
 	NewLabel(info *LabelInfo) core.Result
+
+	// Generate a new label that will be used with the provided block.
+	// This should create a new label with a unique name, but it SHOULDN'T add
+	// it to the labels database (does not call NewLabel).
+	GenerateLabel(block *BasicBlockInfo) *LabelInfo
 }
--- usm64/managers/labels.go
+++ usm64/managers/labels.go
@@ -1,6 +1,9 @@
 package managers
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+
 	"alon.kr/x/usm/core"
 	"alon.kr/x/usm/gen"
 )
@@ -18,6 +21,30 @@ func (m *LabelMap) GetLabel(name string) *gen.LabelInfo {
 func (m *LabelMap) NewLabel(label *gen.LabelInfo) core.Result {
 	(*m)[label.Name] = label
 	return nil
+}
+
+func generateRandomLabelName() (string, error) {
+	b := make([]byte, 4)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return ".L" + hex.EncodeToString(b), nil
+}
+
+func (m *LabelMap) GenerateLabel(block *gen.BasicBlockInfo) *gen.LabelInfo {
+	name, err := generateRandomLabelName()
+	for err != nil || m.GetLabel(name) != nil {
+		name, err = generateRandomLabelName()
+	}
+
+	label := &gen.LabelInfo{
+		Name:       name,
+		BasicBlock: block,
+	}
+
+	return label
 }
 
 func NewLabelManager() gen.LabelManager {
