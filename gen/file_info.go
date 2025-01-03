5ebee08eecbe9d70a08f913acf2b24dec07435e1
Alon Krymgand <me@alon.kr>
2025-01-04 00:01:36 +0200

    Added `String` method for `gen.FileInfo`

--- gen/file_info.go
+++ gen/file_info.go
@@ -3,3 +3,11 @@ package gen
 type FileInfo struct {
 	Functions []*FunctionInfo
 }
+
+func (i *FileInfo) String() string {
+	s := ""
+	for _, function := range i.Functions {
+		s += function.String() + "\n"
+	}
+	return s
+}
