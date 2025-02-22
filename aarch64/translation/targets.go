c55173c81714f52aee29439c9d152364dbe071de
Alon Krymgand <me@alon.kr>
2025-02-22 14:35:36 +0200

    Added basic aarch64 target translations

---
+++ aarch64/translation/targets.go
@@ -0,0 +1,33 @@
+package aarch64translation
+
+import (
+	"fmt"
+
+	"alon.kr/x/aarch64codegen/registers"
+	"alon.kr/x/list"
+	"alon.kr/x/usm/core"
+	"alon.kr/x/usm/gen"
+)
+
+func AssertTargetsExactly(
+	info *gen.InstructionInfo,
+	count int,
+) core.ResultList {
+	if len(info.Targets) != count {
+		return list.FromSingle(core.Result{
+			{
+				Type:     core.ErrorResult,
+				Message:  fmt.Sprintf("Expected %d targets", count),
+				Location: info.Declaration,
+			},
+		})
+	}
+
+	return core.ResultList{}
+}
+
+func TargetToAarch64GPRegister(
+	target *gen.TargetInfo,
+) (registers.GPRegister, core.ResultList) {
+	return RegisterToAarch64GPRegister(target.Register)
+}
