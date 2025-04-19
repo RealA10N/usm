8840169841cead6c886e55e238788882b8e2ab4f
Alon Krymgand <me@alon.kr>
2025-04-19 16:01:40 +0300

    Added `NewFunctionCodegenContext` function

--- aarch64/codegen/function.go
+++ aarch64/codegen/function.go
@@ -9,5 +9,26 @@ type FunctionCodegenContext struct {
 
 	// The offset of each basic block in the function, relative to the function
 	// entry point.
-	BlockOffsets map[*gen.BasicBlockInfo]uint64
+	BasicBlockOffsets map[*gen.BasicBlockInfo]uint64
+}
+
+func NewFunctionCodegenContext(
+	fileCtx *FileCodegenContext,
+	function *gen.FunctionInfo,
+) *FunctionCodegenContext {
+	basicBlocks := function.CollectBasicBlocks()
+	basicBlockOffsets := make(map[*gen.BasicBlockInfo]uint64, len(basicBlocks))
+
+	offset := uint64(0)
+	for _, block := range basicBlocks {
+		basicBlockOffsets[block] = offset
+
+		// In AArch64, each instruction is of constant size of 4 bytes.
+		offset += uint64(block.Size()) * 4
+	}
+
+	return &FunctionCodegenContext{
+		FileCodegenContext: fileCtx,
+		BasicBlockOffsets:  basicBlockOffsets,
+	}
 }
