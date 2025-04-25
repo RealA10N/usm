2e5c3dd51cca362547a00802f8d66cf1a28bceb7
Alon Krymgand <me@alon.kr>
2025-04-25 14:42:56 +0300

    Added sanity `add` instruction tests

---
+++ aarch64/isa/add_test.go
@@ -0,0 +1,117 @@
+package aarch64isa_test
+
+import (
+	"testing"
+
+	"fmt"
+
+	"alon.kr/x/aarch64codegen/immediates"
+	"alon.kr/x/aarch64codegen/instructions"
+	"alon.kr/x/aarch64codegen/registers"
+	aarch64codegen "alon.kr/x/usm/aarch64/codegen"
+	aarch64isa "alon.kr/x/usm/aarch64/isa"
+	aarch64managers "alon.kr/x/usm/aarch64/managers"
+	"alon.kr/x/usm/core"
+	"alon.kr/x/usm/gen"
+	"alon.kr/x/usm/lex"
+	"alon.kr/x/usm/parse"
+	"github.com/stretchr/testify/assert"
+)
+
+func buildInstructionFromSource(
+	t *testing.T,
+	def gen.InstructionDefinition,
+	src string,
+) aarch64codegen.Instruction {
+	srcView := core.NewSourceView(src)
+
+	tokenizer := lex.NewTokenizer()
+	tkns, err := tokenizer.Tokenize(srcView)
+	assert.NoError(t, err)
+
+	tknsView := parse.NewTokenView(tkns)
+	p := parse.NewInstructionParser()
+	node, result := p.Parse(&tknsView)
+	assert.Nil(t, result)
+
+	ctx := aarch64managers.NewGenerationContext().
+		NewFileGenerationContext(srcView.Ctx()).
+		NewFunctionGenerationContext()
+
+	generator := gen.NewInstructionGenerator()
+	baseInfo, results := generator.Generate(ctx, node)
+	assert.True(t, results.IsEmpty())
+	assert.NotNil(t, baseInfo)
+
+	baseInst, results := def.BuildInstruction(baseInfo)
+	assert.True(t, results.IsEmpty())
+	assert.NotNil(t, baseInst)
+
+	inst, ok := baseInst.(aarch64codegen.Instruction)
+	assert.True(t, ok)
+
+	return inst
+}
+
+func assertExpectedCodegen(
+	t *testing.T,
+	def gen.InstructionDefinition,
+	expected instructions.Instruction,
+	src string,
+) {
+	inst := buildInstructionFromSource(t, def, src)
+
+	generationContext := &aarch64codegen.InstructionCodegenContext{}
+	code, results := inst.Generate(generationContext)
+	assert.True(t, results.IsEmpty())
+
+	assert.Equal(t, expected.Binary(), code.Binary())
+}
+
+func TestAddExpectedCodegen(t *testing.T) {
+	def := aarch64isa.NewAddInstructionDefinition()
+
+	testCases := []struct {
+		src      string
+		expected instructions.Instruction
+	}{
+		{
+			"%x0 = add %x1 %x2\n",
+			instructions.NewAddShiftedRegister(
+				registers.GPRegisterX0,
+				registers.GPRegisterX1,
+				registers.GPRegisterX2,
+			),
+		},
+		{
+			"%xzr = add %xzr %xzr\n",
+			instructions.NewAddShiftedRegister(
+				registers.GPRegisterXZR,
+				registers.GPRegisterXZR,
+				registers.GPRegisterXZR,
+			),
+		},
+		{
+			"%x0 = add %x1 $12 #1234\n",
+			instructions.NewAddImmediate(
+				registers.GPorSPRegisterX0,
+				registers.GPorSPRegisterX1,
+				immediates.Immediate12(1234),
+			),
+		},
+		{
+			"%sp = add %sp $12 #0xfff\n",
+			instructions.NewAddImmediate(
+				registers.GPorSPRegisterSP,
+				registers.GPorSPRegisterSP,
+				immediates.Immediate12(0xfff),
+			),
+		},
+	}
+
+	for idx, testCase := range testCases {
+		t.Run(fmt.Sprint(idx), func(t *testing.T) {
+			assertExpectedCodegen(t, def, testCase.expected, testCase.src)
+		})
+	}
+}
