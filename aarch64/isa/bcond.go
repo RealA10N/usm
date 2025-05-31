b6b9424cbfb1c2babe219b4bdc33e7b48ad35dda
Alon Krymgand <me@alon.kr>
2025-05-31 10:38:55 +0300

    Fixed `b.cond`

--- aarch64/isa/bcond.go
+++ aarch64/isa/bcond.go
@@ -12,16 +12,36 @@ import (
 
 type Bcond struct {
 	Condition immediates.Condition
-	Target    *gen.LabelInfo
 }
 
-func (b Bcond) Operator() string {
+func (b Bcond) Target(
+	info *gen.InstructionInfo,
+) (*gen.LabelInfo, core.ResultList) {
+	results := aarch64translation.AssertArgumentsExactly(info, 1)
+	if !results.IsEmpty() {
+		return nil, results
+	}
+
+	target, results := aarch64translation.ArgumentToLabelInfo(info.Arguments[0])
+	if !results.IsEmpty() {
+		return nil, results
+	}
+
+	return target, core.ResultList{}
+}
+
+func (b Bcond) Operator(*gen.InstructionInfo) string {
 	return "b." + b.Condition.String()
 }
 
-func (b Bcond) PossibleNextSteps() (gen.StepInfo, core.ResultList) {
+func (b Bcond) PossibleNextSteps(info *gen.InstructionInfo) (gen.StepInfo, core.ResultList) {
+	target, results := b.Target(info)
+	if !results.IsEmpty() {
+		return gen.StepInfo{}, results
+	}
+
 	return gen.StepInfo{
-		PossibleBranches: []*gen.LabelInfo{b.Target},
+		PossibleBranches: []*gen.LabelInfo{target},
 		PossibleContinue: true,
 	}, core.ResultList{}
 }
@@ -29,7 +49,12 @@ func (b Bcond) PossibleNextSteps() (gen.StepInfo, core.ResultList) {
 func (b Bcond) Generate(
 	ctx *aarch64codegen.InstructionCodegenContext,
 ) (instructions.Instruction, core.ResultList) {
-	targetBasicBlock := b.Target.BasicBlock
+	target, results := b.Target(ctx.InstructionInfo)
+	if !results.IsEmpty() {
+		return nil, results
+	}
+
+	targetBasicBlock := target.BasicBlock
 	targetOffset := ctx.BasicBlockOffsets[targetBasicBlock]
 	currentOffset := ctx.InstructionOffsetInFunction
 	offset, err := aarch64translation.Uint64DiffToOffset19Align4(targetOffset, currentOffset)
@@ -52,35 +77,20 @@ func (b Bcond) Generate(
 	return instruction, core.ResultList{}
 }
 
-type BcondDefinition struct {
-	Condition immediates.Condition
-}
-
-func (d BcondDefinition) BuildInstruction(
+func (b Bcond) Validate(
 	info *gen.InstructionInfo,
-) (gen.BaseInstruction, core.ResultList) {
+) core.ResultList {
 	results := core.ResultList{}
 
-	curResults := aarch64translation.AssertArgumentsExactly(info, 1)
+	_, curResults := b.Target(info)
 	results.Extend(&curResults)
 
 	curResults = aarch64translation.AssertTargetsExactly(info, 0)
 	results.Extend(&curResults)
 
 	if !results.IsEmpty() {
-		return nil, results
+		return results
 	}
 
-	target, curResults := aarch64translation.ArgumentToLabelInfo(info.Arguments[0])
-	results.Extend(&curResults)
-
-	if !results.IsEmpty() {
-		return nil, results
-	}
-
-	return Bcond{Condition: d.Condition, Target: target}, core.ResultList{}
-}
-
-func NewBcondInstructionDefinition(condition immediates.Condition) gen.InstructionDefinition {
-	return BcondDefinition{Condition: condition}
+	return core.ResultList{}
 }
